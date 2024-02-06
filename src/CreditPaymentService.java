fbcd15d4abcbcd054e52ad8a313db4bd1c73dc83
Irina D <RabotaYd2021@yandex.ru>
2024-02-06 13:53:57 +0300

    Change text Main.java and loanPeriod

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -2,9 +2,7 @@ public class CreditPaymentService {
     public int calculate(int amountCredit, int loanPeriod, double interestRate) {
         double payment;
 
-        if (loanPeriod >= 6) {
-            interestRate = interestRate / 12 / 100;
-        }
+        interestRate = interestRate / 12 / 100;
 
         payment = amountCredit * (interestRate * Math.pow((1 + interestRate), loanPeriod) / (Math.pow((1 + interestRate), loanPeriod) - 1));
         return (int) payment;
--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,10 @@ public class Main {
         CreditPaymentService service = new CreditPaymentService();
 
         System.out.println("1. Ежемесячный платеж: ");
-        System.out.println(service.calculate(1_000_000, 6, 9.99));
+        System.out.println(service.calculate(1_000_000, 12, 9.99));
 
         System.out.println("2. Ежемесячный платеж: ");
-        System.out.println(service.calculate(1_000_000, 8, 9.99));
+        System.out.println(service.calculate(1_000_000, 24, 9.99));
 
         System.out.println("3. Ежемесячный платеж: ");
         System.out.println(service.calculate(1_000_000, 36, 9.99));
